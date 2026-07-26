a9b7dc59d37ccf77a60db0c6f2b92d7b00adf32b
Computhink-CS145 <cs145@computhink.com.sg>
2026-07-26 08:42:51 +0000

    Jul 26, 2026, 8:42 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -8,7 +8,7 @@ function setup(){
  function draw() {
     fill(shapecoulour);
     circle(300,200,300);
-    fill(random(255),random(255,))
+    fill(random(255),random(255),random)
  }
 
  function mousePressed() {
