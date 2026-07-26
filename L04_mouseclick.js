6d7e0f6c347cc960707a56878ca5c5d6ec2b8454
Computhink-CS145 <cs145@computhink.com.sg>
2026-07-26 09:15:08 +0000

    Jul 26, 2026, 9:15 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -21,5 +21,5 @@ function setup(){
 //     background(220);
 
 function mouseDragged(){
-    circle(mouseX+random(-25,25),mouse)                
+    circle(mouseX+random(-25,25),mouseY+random)                
 }
