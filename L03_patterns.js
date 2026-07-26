52df04b4fcba597155b70c649dcee1767d0933c0
Computhink-CS145 <cs145@computhink.com.sg>
2026-07-26 08:24:25 +0000

    Jul 26, 2026, 8:24 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -21,5 +21,6 @@ function draw() {
   let startY = (height - totalwidth) / 2 + circlediamiter / 2;
 
   for (let row = 0; row < numcircles; row++)
-    let rowY = startY + (circlediamiter * row)
+    let rowY = startY + (circlediamiter * row);
+    
 }
